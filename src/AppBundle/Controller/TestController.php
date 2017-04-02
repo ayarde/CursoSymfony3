1c78ad5ec3770944eb8b20ea61fc64b81d25d811
ayarde <adrian.ayarde@gmail.com>
2017-04-02 20:26:28 -0300

    GET and POST variable, Create bundles, Templates and Block, Variables,conditionala and loop

--- src/AppBundle/Controller/TestController.php
+++ src/AppBundle/Controller/TestController.php
@@ -17,9 +17,24 @@ class TestController extends Controller
 
     public function indexAction(Request $request, $name, $page)
     {
+        //return $this->redirect($request.getBaseUrl()."/hello-world?hello=true");
         // replace this example code with whatever you need
+        /*var_dump($request->query->get("hello"));
+        var_dump($request->get("hola-post"));
+        die();*/
+        $products = array(
+                    array("product"=>"Console 1","price"=>2),
+                    array("product"=>"Console 2","price"=>3),
+                    array("product"=>"Console 3","price"=>4),
+                    array("product"=>"Console 4","price"=>5),
+                    array("product"=>"Console 5","price"=>6)
+                    );
+        $fruits = array("apple" => "golden","pear" => "delicius");
+
         return $this->render('AppBundle:tests:index.html.twig', [
             'text' => $name." - ".$page,
+            'products' => $products,
+            'fruits' => $fruits
         ]);
     }
 
